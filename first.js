5d911436dc760cc4a228278a7a90d0b7dba429ad
5Knight2 <pratiktarale258@gmail.com>
2023-06-06 16:39:07 +0530

    express2

---
+++ first.js
@@ -0,0 +1,24 @@
+
+const express=require('express')
+const bodyparser=require('body-parser');
+
+console.log('hi again')
+
+const app=express();
+app.use(bodyparser.urlencoded({extended:false}));
+app.get('/add-product',(req,res,next)=>{
+    res.send("<html><body><form action='/add-product' method='post'><input type='text' name='message' ><button type=submit>Send</button></form></body></html>");
+});
+
+app.post('/add-product',(req,res,next)=>{
+    console.log(req.body);
+   
+    res.redirect('/');
+   
+});
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>product added<h1>');
+})
+
+app.listen(8000);
