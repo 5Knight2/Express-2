3569efd07174395cd11e874080fef5e60077f734
5Knight2 <79683322+5Knight2@users.noreply.github.com>
2023-06-06 16:45:30 +0530

    Update first.js

--- first.js
+++ first.js
@@ -7,7 +7,7 @@ console.log('hi again')
 const app=express();
 app.use(bodyparser.urlencoded({extended:false}));
 app.get('/add-product',(req,res,next)=>{
-    res.send("<html><body><form action='/add-product' method='post'><input type='text' name='message' ><button type=submit>Send</button></form></body></html>");
+    res.send("<html><body><form action='/add-product' method='post'><input type='text' name='Product_Name' ><input type='number' name='Product_count' ><button type=submit>Send</button></form></body></html>");
 });
 
 app.post('/add-product',(req,res,next)=>{
